f699d4772ecaca16b7b57474af459a5e834ca507
Daniel <Daniel.teixeirari@gmail.com>
2020-06-13 22:38:47 -0300

    profile controller

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,11 +4,14 @@ const connection = require('./database/connection')
 
 const ongsController = require('./controllers/ongs_controller')
 const incidentsController = require('./controllers/incidents_controller')
+const profileController = require('./controllers/profile_controller')
 
 const routes = express.Router();
 
 routes.get('/ongs', ongsController.index)
 routes.get("/incidents",incidentsController.index )
+
+routes.get("/profiles", profileController.index);
 
 routes.post('/ongs', ongsController.create)
 routes.post('/incidents', incidentsController.create)
