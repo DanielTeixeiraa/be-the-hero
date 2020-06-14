2c68653c67656e26409f76dbbf553ca57d4ce36b
Daniel <Daniel.teixeirari@gmail.com>
2020-06-13 23:04:15 -0300

    api complete

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,6 +5,7 @@ const connection = require('./database/connection')
 const ongsController = require('./controllers/ongs_controller')
 const incidentsController = require('./controllers/incidents_controller')
 const profileController = require('./controllers/profile_controller')
+const sessionController = require('./controllers/session_controller')
 
 const routes = express.Router();
 
@@ -15,6 +16,7 @@ routes.get("/profiles", profileController.index);
 
 routes.post('/ongs', ongsController.create)
 routes.post('/incidents', incidentsController.create)
+routes.post('/sessions', sessionController.create)
 
 routes.delete('/incidents/:id', incidentsController.delete)
 
