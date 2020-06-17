4ad5eb5984a28a418ece9a68eb2fb3ec8b7e399c
Daniel <Daniel.teixeirari@gmail.com>
2020-06-17 20:09:38 -0300

    New incident page

---
+++ web/src/pages/NewIncident/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+
+import './styles.css'
+import Logo from '../../assets/logo.svg'
+
+export default function NewIncident(){
+  return(
+    <div className="new-incident">
+    <div className="content">
+      <section>
+        <img src={Logo} alt="Be The Hero" />
+
+        <h1>Cadastrar novo caso</h1>
+        <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+        <Link className="black-link" to="/">
+        <FiArrowLeft size={16} color="#e02041" />
+        Voltar para home
+        </Link>
+      </section>
+
+      <form >
+    <input type="text" placeholder="Titulo do caso"/>
+    <textarea placeholder="Descrição" />
+    <input type="number" placeholder="Valor em reais"/>
+      <button className="button" type="submit"></button>
+      </form>
+    </div>
+  </div>
+  )
+}
